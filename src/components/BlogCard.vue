4a3687f8762df3ae0a57e033a76c98de3fff6d9c
JoeCalvi <joecalvi@me.com>
2023-02-23 15:43:01 -0700

    blog card stubbed

---
+++ src/components/BlogCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="BlogCard">
+        <div class="card blog-post blog-img my-3">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-12 d-flex align-items-center justify-content-between">
+                <h5>Title</h5>
+                <img src="https://pbs.twimg.com/profile_images/704028352152215553/8JKiR5mp_400x400.jpg" alt="" class="creator-img rounded-circle">
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    setup(){
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,18 @@
 <template>
-  <h1>Here's my data baby</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-4" v-for="b in blogs">
+        
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { onMounted, computed } from 'vue';
 import Pop from '../utils/Pop.js';
 import { blogsService } from '../services/BlogsService.js'
+import { AppState } from '../AppState.js';
 
 export default {
   setup() {
@@ -19,11 +26,30 @@ export default {
 
     onMounted(() => getBlogs())
 
-    return {}
+    return {
+      blogs: computed(() => AppState.blogs)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
+.blog-img{
+  height: 40vh;
+  width: 100%;
+  object-fit: cover;
+}
+
+.blog-post{
+  background-image: url('https://pbs.twimg.com/profile_images/704028352152215553/8JKiR5mp_400x400.jpg');
+  object-fit: cover;
+  text-shadow: 1px 2px 2px rgb(202, 219, 76);
+}
+
+.creator-img{
+  height: 10vh;
+  width: 10vh;
+}
 
 </style>
