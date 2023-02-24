337949e09d247e961df5d9d5730d8c6b23f19f92
JoeCalvi <joecalvi@me.com>
2023-02-23 17:06:04 -0700

    individual blogs drawing

---
+++ src/components/BlogContent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="BlogContent">
+        <div class="container-fluid">
+            <div v-if="blog" class="row m-0">
+                <div class="col-12 m-auto">
+                    <img :src="blog.imgUrl" alt="">
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-10 mt-3">
+                        <h3 class="text-center">{{ blog.title }}</h3>
+                        <p>{{ blog.body }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+import { computed } from 'vue'
+import { AppState } from '../AppState.js';
+import { Blog } from '../models/Blog.js';
+
+export default {
+    props: {
+        blog: {
+            type: Blog,
+            required: true
+        } 
+    },
+    setup(){
+        return {
+            blog: computed(() => AppState.blog)
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+img{
+    object-fit: cover;
+    width: 100%;
+}
+
+</style>
--- src/pages/BlogPage.vue
+++ src/pages/BlogPage.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="BlogPage">
-<h1>this is the blog you selected</h1>
-
+       <BlogContent :blog="blog" />
     </div>
 </template>
 
 
 <script>
 import { useRoute } from 'vue-router';
+import { Blog } from '../models/Blog.js';
 import { blogsService } from '../services/BlogsService.js';
 
 export default {
+    
     setup(){
         const route = useRoute()
 
